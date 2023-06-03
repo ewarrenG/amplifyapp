0e101221fbbce35e5958ac271561c85b1cdb6017
Elliot Glasenk <elliotglasenk@Elliots-MBP.fios-router.home>
2023-06-03 12:38:31 -0400

    changes for v3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>Hello from V3</h1>
       </header>
     </div>
   );
